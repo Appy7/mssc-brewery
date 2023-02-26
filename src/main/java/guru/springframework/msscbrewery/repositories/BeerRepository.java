f3c64f065675815c4de626d9fd4a766441c5e86c
Appy7 <aparna.samal32@gmail.com>
2023-02-26 22:21:02 +0530

    added repo class

---
+++ src/main/java/guru/springframework/msscbrewery/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbrewery.repositories;
+
+import guru.springframework.msscbrewery.Domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
